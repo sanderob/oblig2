09852016e2b5bd04e50e10afc4382e5e9fa55e02
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2021-02-04 00:35:20 +0100

    Visual adjustments
    Removed empty lines, minor visual adjustments

--- src/main/java/Membership.java
+++ src/main/java/Membership.java
@@ -1,7 +1,19 @@
 public abstract class Membership {
 
-
     public abstract int registerPoints(int bonusPointBalance, int newPoints);
-
     public abstract String getMembershipName();
+
+
+    public static Membership createGMembership() {
+        return new GoldMembership();
+    }
+
+    public static Membership createSMembership() {
+        return new SilverMembership();
+    }
+
+    public static Membership createBMembership() {
+        return new BasicMembership();
+    }
+
 }
